b86a89f377e92839b45920f97b9c04483e01519f
bom2zzang <bom2zzang@me.com>
2019-09-08 18:20:58 +0900

    la

--- day_04/export_file.py
+++ day_04/export_file.py
@@ -1,3 +1,4 @@
+'''
 #!/usr/bin/env python3
 import csv
 import MySQLdb
@@ -20,6 +21,33 @@ filewriter.writerow(header)
 c.execute("""SELECT *
     FROM Suppliers
     WHERE Cost > 700.0;""")
+rows = c.fetchall()
+for row in rows:
+    filewriter.writerow(row)
+ 
+ #cmd>python 5db_mysql_write_to_file.py 5output.csv
+ '''
+ #!/usr/bin/env python3
+import csv
+import pymysql
+import sys
+
+# Path to and name of a CSV output file
+output_file = sys.argv[1]
+
+# Connect to a MySQL database
+#con = MySQLdb.connect(host='localhost', port=13306, db='my_suppliers', user='root', passwd='1111')
+con = pymysql.connect(host='localhost', port=3306, db='studydb', user='root', passwd='1111')
+c = con.cursor()
+
+# Create a file writer object and write the header row
+filewriter = csv.writer(open(output_file, 'w', newline=''), delimiter=',')
+header = ['bno','title','conts','file','date']
+filewriter.writerow(header)
+
+# Query the Suppliers table and write the output to a CSV file
+c.execute("""SELECT *
+    FROM board;""")
 rows = c.fetchall()
 for row in rows:
     filewriter.writerow(row)
